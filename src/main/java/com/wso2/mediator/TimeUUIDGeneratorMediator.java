05ff5740d3d515d77cf206f28c6416d2af466b6f
Vanjikumaran <vanjikumaran@gmail.com>
2018-06-04 23:06:34 -0700

    intial Change for Time baed UUID

--- src/main/java/com/wso2/mediator/TimeUUIDGeneratorMediator.java
+++ src/main/java/com/wso2/mediator/TimeUUIDGeneratorMediator.java
@@ -1,3 +1,5 @@
+package com.wso2.mediator;
+
 import com.fasterxml.uuid.Generators;
 import org.apache.synapse.MessageContext; 
 import org.apache.synapse.mediators.AbstractMediator;
@@ -6,7 +8,7 @@ public class TimeUUIDGeneratorMediator extends AbstractMediator {
 	
 	public static final String CURRENT_TIME_UUID = "uri.var.uuid";
 
-	public boolean mediate(MessageContext messageContext) { 
+	public boolean mediate(MessageContext messageContext) {
 		messageContext.setProperty(CURRENT_TIME_UUID, Generators.timeBasedGenerator().generate().toString());
 		return true;
 	}
